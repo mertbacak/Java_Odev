c38f9d89787171ce247ee2b5404a70ab47052b41
mert.bacak <mertbacak250@gmail.com>
2022-08-05 14:52:09 +0300

    Deneme

---
+++ Sinav_Notu_Hesaplamasi/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+        while (true) {
+        System.out.print("Lütfen Adınızı Soyadınızı Giriniz=");
+        String adsoyad=input.nextLine();
+        System.out.print("Lütfen Quiz Notunuzu Giriniz=");
+        int quiz=input.nextInt();
+        System.out.print("Lütfen Vize Notunuzu Giriniz=");
+        int vize=input.nextInt();
+        System.out.print("Lütfen Final Notunuzu Giriniz=");
+        int finals=input.nextInt();
+
+        double ortalama=((quiz*0.1)+(vize*0.3)+(finals*0.6));
+
+            if (ortalama >= 85 && ortalama <= 100) {
+                System.out.println("Sayın" + " " + adsoyad + " " + "Not Ortalamanız AA");
+                System.out.println("Sınıfta Geçtiniz");
+                break;
+            } else if (ortalama < 85 && ortalama >= 70) {
+                System.out.println("Sayın" + " " + adsoyad + " " + "Not Ortalamanız BB");
+                System.out.println("Sınıfta Geçtiniz");
+                break;
+            } else if (ortalama < 70 && ortalama >= 60) {
+                System.out.println("Sayın" + " " + adsoyad + " " + "Not Ortalamanız CC");
+                System.out.println("Sınıfta Geçtiniz");
+                break;
+            } else if (ortalama < 60 && ortalama >= 45) {
+                System.out.println("Sayın" + " " + adsoyad + " " + "Not Ortalamanız DD");
+                System.out.println("Sınıfta Geçtiniz");
+                break;
+            } else if (ortalama<45 && ortalama <=0) {
+
+                System.out.println("Sayın" + " " + adsoyad + " " + "Not Ortalamanız FF");
+                System.out.println("Sınıfta kaldınız");
+                break;
+            }
+            else {
+                System.out.println("Lütfen geçerli bir Quiz,Vize,Final Notu giriniz");
+            }
+        }
+    }
+}
